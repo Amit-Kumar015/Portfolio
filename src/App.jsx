e83cc21f93568afa614bb63b3b1395c3bcc84c26
Amit Kumar <amitk200415@gmail.com>
2025-02-08 12:26:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl">hello</div>
+    </>
+  )
+}
+
+// leave a message - later
+// profile
+// aboutme 
+// experience
+// projects
+// blogs
+// skills
+// contact
+// connect
+// footer
+
+export default App

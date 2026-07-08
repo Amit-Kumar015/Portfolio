8371e74ebce87758e2498f1c1ac0e9af9ea32272
Amit Kumar <amitk200415@gmail.com>
2026-07-09 03:12:16 +0530

    remove comments

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,30 +1,3 @@
-// export const Header = () => {
-//   return (
-//     <div className="flex justify-center items-center fixed w-full z-10 top-3">
-//       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-//         <a href="#" className="nav-item">
-//           Home
-//         </a>
-//         <a href="#experience" className="nav-item">
-//           Experience
-//         </a>
-//         <a href="#projects" className="nav-item">
-//           Projects
-//         </a>
-//         <a href="#about" className="nav-item">
-//           About
-//         </a>
-//         <a
-//           href="#contact"
-//           className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-//         >
-//           Contact
-//         </a>
-//       </nav>
-//     </div>
-//   );
-// };
-
 export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed w-full z-50 top-3 px-4">
